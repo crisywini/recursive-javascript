b8c074c522bace214bd1b39d6a6bdfe0db4766b7
Crisi <harmaharcri@hotmail.com>
2021-08-05 18:42:36 -0500

    Solved the error in the fibonacci file

--- fibonacci.js
+++ fibonacci.js
@@ -1,7 +1,9 @@
-funtion fibonacci (n){
+function fibonacci(n){
 
-if(n==1||n==2){
-return 1;
+	if(n==1||n==2){ 
+		return 1;
+	}
+	return fibonacci(n-1) + fibonacci(n-2);
 }
-return fibonacci(n-1)+ fibonacci(n-2)
-}
+
+console.log(fibonacci(1));
